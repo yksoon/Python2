21a5828955e54c3087bc174dee73f07c8c271aa9
yksoon <ksyong1990@gmail.com>
2020-04-26 18:28:00 +0900

    Y_20200426

--- stubs.v4/owHfFmESRmUnT1Mvw_onkI_A2yHoCfQ_6oalI6-zX24=/_pocketfft_internal.cp37-win_amd64.pyi
+++
@@ -1,7 +0,0 @@
-__doc__ = None
-__file__ = 'c:\\users\\user\\appdata\\local\\programs\\python\\python37\\lib\\site-packages\\numpy\\fft\\_pocketfft_internal.cp37-win_amd64.pyd'
-__name__ = 'numpy.fft._pocketfft_internal'
-__package__ = 'numpy.fft'
-def execute():
-    pass
-
